f1db9638b1f0e8e6544c4471f87edb513fce02fc
jiangjieqim <jiangjieqim@hotmail.com>
2026-03-12 13:07:36 +0800

    取消脚本

--- mongodb/bin/startmongodb.bat
+++ mongodb/bin/startmongodb.bat
@@ -1,4 +1,9 @@
-mkdir db
-start mongod.exe --config ".\mongo.config"
+::	mkdir db
+::	start mongod.exe --config ".\mongo.config"
 @REM echo mongondDB succeed...
 @REM pause
+
+::mkdir db
+::mongod --dbpath .\db --port 27018 --quiet
+
+
